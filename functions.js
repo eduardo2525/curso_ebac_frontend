98d7b0e3468685890833c76fde773a4ca67938f8
eduardo <marketingdoeduardo@gmail.com>
2024-04-10 13:11:55 -0300

    Exercicio de typescript

---
+++ functions.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+const saudacao = (nome) => `Olá ${nome}`;
+console.log(multiplicar(2, 5));
+console.log(saudacao("Eduardo"));
---
+++ functions.ts
@@ -0,0 +1,8 @@
+function multiplicar(n1: number, n2:number): number {
+    return n1 * n2;
+}
+
+const saudacao = (nome: string) => `Olá ${nome}`;
+
+console.log(multiplicar(2, 5));
+console.log(saudacao("Eduardo"))
